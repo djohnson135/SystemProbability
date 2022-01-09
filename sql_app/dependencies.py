513238c06ad4dbeecd14bfa5525dc9f2c602a5c9
djohnson135 <47255628+djohnson135@users.noreply.github.com>
2022-01-08 22:54:32 -0600

    Organized the routers and the crud functions. Added a dependency file (#4)
    * adding files
    
    * test
    
    * Organized Crud files
    
    * adding router files
    
    not finished
    
    * router changes
    
    * Added working system router
    
    * changed router file structure
    
    working need to fix and change the schemas next
    
    * Crud and routers finished

---
+++ sql_app/dependencies.py
@@ -0,0 +1,54 @@
+from fastapi import Header, HTTPException
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
+import passlib.hash as _hash
+from sqlalchemy.sql.expression import false
+import jwt as _jwt
+from fastapi import Depends
+import fastapi.security as _security
+from .database import SessionLocal, engine
+import datetime as _dt
+
+from . import models, schemas
+
+# uvicorn.run(app,host="0.0.0.0", port=8000)
+
+
+JWT_SECRET = "myjwtsecret"
+oauth2schema = _security.OAuth2PasswordBearer(tokenUrl="/token/")
+
+
+def create_database():
+    return models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+async def authenticate_user(email: str, password: str, db: Session):
+    user = await get_user_by_email(db = db, email = email)
+    if not user or not user.verify_password(password):
+        return False
+    return user
+
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2schema)):
+    try:
+        payload = _jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        user = db.query(models.User).get(payload["id"])
+    except:
+        raise HTTPException(status_code=401, detail= "Invalid Email or Password")
+    return schemas.User.from_orm(user)
+
+async def create_token(user: models.User):
+    user_obj = schemas.User.from_orm(user)   #from orm takes a model and maps it to the schema. Makes it a schema object
+    token = _jwt.encode(user_obj.dict(), JWT_SECRET)
+    return dict(access_token=token, token_type= "bearer")
+
+async def get_user_by_email(email: str, db: Session):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+
