51ad6c8a6b41651ffbf78d69653d043aa27bc678
djohnson135 <dathanljohnson135@gmail.com>
2022-01-08 22:52:29 -0600

    Crud and routers finished

--- sql_app/__main__.py
+++ sql_app/__main__.py
@@ -1,62 +1,33 @@
 # import uvicorn
-from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
-
-from fastapi.middleware.cors import CORSMiddleware
-import fastapi.security as _security
-from pydantic.schema import model_schema
-
-from sqlalchemy.orm import Session
-from sqlalchemy.sql.expression import update
-
-from sql_app.crud import graph as crud_graph
-from sql_app.crud import system as crud_system
-from sql_app.crud import node as crud_node
-from sql_app.crud import user as crud_user
-# from sql_app.crud import helper as crud_helper
-from . import dependencies, schemas 
-
-from sql_app.routers import user as user_route
-from sql_app.routers import graph as graph_route
-from sql_app.routers import node as node_route
-from sql_app.routers import system as system_route
+from fastapi import FastAPI
+from .dependencies import create_database
+from sql_app.routers.user import router as user_route
+from sql_app.routers.graph import router as graph_route
+from sql_app.routers.graph import get_router as get_graph_route
+from sql_app.routers.node import router as node_route
+from sql_app.routers.node import get_router as get_node_route
+from sql_app.routers.system import router as system_route
 # from .database import SessionLocal, engine
 
 
-dependencies.create_database()
+create_database()
 
 
 app = FastAPI()
 # dependencies=[Depends(dependencies.get_current_user)]
 
-app.include_router(user_route.router)
-app.include_router(system_route.router)
-app.include_router(node_route.get_router)
-app.include_router(node_route.router)
-app.include_router(graph_route.router)
-app.include_router(graph_route.get_router)
+app.include_router(user_route)
+app.include_router(system_route)
+app.include_router(get_node_route)
+app.include_router(node_route)
+app.include_router(graph_route)
+app.include_router(get_graph_route)
 
 @app.get("/api/")
 async def root():
     return {"Message": "System Proabbility Calculator"}
 
-
-@app.post("/users/")
-async def create_user(user: schemas.UserCreate, db: Session = Depends(dependencies.get_db)):
-    db_user = await dependencies.get_user_by_email(email = user.email, db = db)
-    if db_user:
-        raise HTTPException(status_code=400, detail= "Email already in use")
-    users = await crud_user.create_user(user = user, db = db)
-    return await dependencies.create_token(users)
-    
-    
-@app.post("/token/")
-async def generate_token(form_data: _security.OAuth2PasswordRequestForm = Depends(), db: Session = Depends(dependencies.get_db)):
-    user = await dependencies.authenticate_user(email = form_data.username, password = form_data.password, db = db)  #form_data.username refers to email
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid Credentials")
-    return await dependencies.create_token(user)
 
 
 
--- sql_app/routers/user.py
+++ sql_app/routers/user.py
@@ -1,14 +1,36 @@
 from fastapi import APIRouter, Depends, HTTPException
-from ..dependencies import get_current_user
-from ..schemas import User
+from ..dependencies import get_current_user, get_db, create_token, authenticate_user, get_user_by_email
+from ..schemas import User, UserCreate
+from sqlalchemy.orm import Session
+from ..crud import user as crud_user
+import fastapi.security as _security
+
 
 router = APIRouter(
-    prefix="/users/me",
+    # prefix="/users/me",
     tags=["user"],
     # dependencies=[Depends(get_current_user)],
     responses={404: {"description": "Not found"}},
 )
 
-@router.get("/", response_model= User)
+@router.get("/users/me/", response_model= User)
 async def get_user(user: User = Depends(get_current_user)):
     return user
+
+
+
+@router.post("/users/")
+async def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = await get_user_by_email(email = user.email, db = db)
+    if db_user:
+        raise HTTPException(status_code=400, detail= "Email already in use")
+    users = await crud_user.create_user(user = user, db = db)
+    return await create_token(users)
+    
+    
+@router.post("/token/")
+async def generate_token(form_data: _security.OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = await authenticate_user(email = form_data.username, password = form_data.password, db = db)  #form_data.username refers to email
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid Credentials")
+    return await create_token(user)
