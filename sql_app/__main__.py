016f5b299136c587d33b371680de7ba24e718bea
djohnson135 <dathanljohnson135@gmail.com>
2022-01-10 23:38:20 -0600

    Last commit before bed not working commit

--- sql_app/__main__.py
+++ sql_app/__main__.py
@@ -1,8 +1,15 @@
-# import uvicorn
+import uvicorn
 from .dependencies import create_database
-from sql_app.routers.app import app
+# from fastapi import Depends
+import asyncio
 
+from .routers.app import app
+from . import processes
 create_database()
+
+if __name__ == "__main__":
+    uvicorn.run("sql_app.__main__:app", host="0.0.0.0", port=8000, reload=True)
+  
 
 
 @app.get("/api/")
@@ -11,7 +18,21 @@ async def root():
 
 
 
+# try:
+#     loop = asyncio.get_running_loop()
+# except RuntimeError:  # 'RuntimeError: There is no current event loop...'
+#     loop = None
 
+# if loop and loop.is_running():
+#     print('Async event loop already running. Adding coroutine to the event loop.')
+#     tsk = loop.create_task(processes.set_adj_list(graph_id=1))
+#     # ^-- https://docs.python.org/3/library/asyncio-task.html#task-object
+#     # Optionally, a callback function can be executed when the coroutine completes
+#     tsk.add_done_callback(
+#         lambda t: print(f'Task done with result={t.result()}  << return val of main()'))
+# else:
+#     print('Starting new event loop')
+#     asyncio.run(processes.set_adj_list(graph_id=1))
 
-
-
+# asyncio.run(processes.set_adj_list(graph_id=1))
+processes.set_adj_list(graph_id=1)
--- sql_app/processes.py
+++ sql_app/processes.py
@@ -1,24 +1,41 @@
-# from fastapi.exceptions import HTTPException
-# from sqlalchemy.orm import Session
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
 # import passlib.hash as _hash
 # from sqlalchemy.sql.expression import false
 # import jwt as _jwt
 from fastapi import Depends
-# from . import models, schemas
+from .dependencies import get_db
+from . import models, schemas
 import numpy as np
+from .crud import graph
 
-async def calculated_system_probability(adj_list: np.matrix):
-    """[summary]
-    Takes an adjacency list of dictionary pairs and returns the total system probability
-    Args:
-        adj_list (np.matrix): [description]
-    """
-    rows = adj_list.shape[0]
-    cols = adj_list.shape[1]
-    for x in range(0, rows):
-        #
-        for y in range(0, cols):
-            print (adj_list[x,y])
+
+
+class system_probability:
+    adj_list: np.matrix 
+  
+        
+    async def calculated_system_probability(adj_list: np.matrix):
+        """[summary]
+        Takes an adjacency list of dictionary pairs and returns the total system probability
+        Args:
+            adj_list (np.matrix): [description]
+        """
+        rows = adj_list.shape[0]
+        cols = adj_list.shape[1]
+        for x in range(0, rows):
+            #
+            for y in range(0, cols):
+                print (adj_list[x,y])
+        
+        
+
+# async def set_adj_list(graph_id: int, db: Session = Depends(get_db)): #
     
+#     return await graph.get_by_id( graph_id=graph_id, db=db)
 
-
+def set_adj_list(graph_id: int, db: Session = get_db): #
+    db_graph = db.query(models.Graph).filter_by(id = graph_id).first()
+    if db_graph is None:
+        raise HTTPException(status_code=404, detail = "Graph does not exist")
+    return schemas.Graph.from_orm(db_graph)
